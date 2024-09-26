9d021c577e9d474e5abe2df26ca8cf46a45b86d8
Julia_Chu <z3211232001@gmail.com>
2024-09-26 12:19:52 -0700

    completed 02

--- ex03.js
+++ ex03.js
@@ -3,7 +3,8 @@
 // Verify that the Set only keeps unique values by logging it to the console.
 
 const uniqueNumbers = new Set();
-
-// Your code here
-
+uniqueNumbers.add(1)
+uniqueNumbers.add(2)
+uniqueNumbers.add(3)
+uniqueNumbers.add(1) //it's duplicate so not going to stole
 console.log(uniqueNumbers);
